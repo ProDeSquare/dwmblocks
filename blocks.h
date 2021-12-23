0d36e3c26b0cf0c95bd450f106979fd0d0278747
Hamza Mughal <hamza@prodesquare.com>
2021-12-24 02:39:39 +0500

    clean blocks

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
-	{"  ", 	"kernel",					0,					0},
-	{"  ", 	"memory",					4,					0},
-	{"  ", 	"blevel",					0,					11},
+	/* {"  ", 	"kernel",					0,					0}, */
+	/* {"  ", 	"pacc",						0,					0}, */
+	/* {"  ", 	"upt",						5,					0}, */
+	/* {"  ", 	"memory",					4,					0}, */
+	/* {"  ", 	"blevel",					0,					11}, */
 	{" ",  		"volume",					0,					10},
 	{" ",  		"battery",					2,					0},
 	{" ",  		"clock",					5,					0},
@@ -12,4 +14,4 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '/';
+static char delim = '|';
