9f3ec6920d193ac8b0fd5811726e460512bfbafb
Hamza Mughal <hamza@prodesquare.com>
2022-03-13 01:34:51 +0500

    updated blocks

--- blocks.h
+++ blocks.h
@@ -1,12 +1,16 @@
 static const Block blocks[] = {
 	/*Icon*/		/*Command*/					/*Update Interval*/	/*Update Signal*/
+	{" KERNEL:", 	"kernel",					0,					0},
+	{" UPTIME:", 	"upt",                      0,					0},
 	{" MEMORY:", 	"memory",					4,					0},
 	{" ",	  		"volume",					0,					10},
 	{" ",			"battery",					2,					0},
 	{" CPU:",		"cpu",						5,					0},
-	{" ",			"clock",					5,					0},
+	{"",			"clock",					5,					0},
 	{" ",			"internet",					5,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '\0';
+static char delim[] = "\0";
+static unsigned int delimLen = 5;
+
