2dbb274e9fbb09a7d88566b74f8ba1aae333e543
Hamza Mughal <hamza@prodesquare.com>
2023-01-24 05:31:51 +0500

    adapting three letter statusbar labels

--- blocks.h
+++ blocks.h
@@ -1,17 +1,21 @@
 static const Block blocks[] = {
+    /* Crypto */
+	{"",           "price btc Bitcoin BTC: usd",	0,					0},
+	// {"",           "price eth Ethereum ETH: usd",	0,					0},
+	// {"",           "price xmr Monero XMR: usd",     0,					0},
+
 	/*Icon*/		/*Command*/					/*Update Interval*/	/*Update Signal*/
-	{" ",           "price btc Bitcoin BTC: usd",	0,					0},
-	{" KERNEL:", 	"kernel",					0,					0},
-	{" UPTIME:", 	"upt",                      5,					0},
-	{" MEMORY:", 	"memory",					5,					0},
-	{" ",	  		"volume",					0,					10},
-	{" ",			"battery",					2,					0},
-	{" CPU:",		"cpu",						5,					0},
-	{" ",			"clock",					5,					0},
-	{" ",			"internet",					5,					0},
+	{"KER:",        "kernel",					0,					0},
+	{"UPT:",        "upt",                      5,					0},
+	{"MEM:",        "memory",					5,					0},
+	{"",	  		"volume",					0,					10},
+	{"",			"battery",					2,					0},
+	{"CPU:",		"cpu",						5,					0},
+	{"",			"clock",					5,					0},
+	{"",			"internet",					5,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "\0";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
 
